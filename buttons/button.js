f592b81147aa037259c7e209e4c3bdbb29aaa977
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-03-31 23:13:54 -0400

    Update button.js
    Make audio play on click and fade out a little bit

--- buttons/button.js
+++ buttons/button.js
@@ -1,9 +1,25 @@
 const button1 = document.querySelector("#button-one");
 const button2 = document.querySelector("#button-two");
+const clickAudio = document.querySelector("#click-audio");
 
 button1.addEventListener("mousedown", (e) => {
+  const clone = clickAudio.cloneNode();
   button1.classList.add("clicked");
+  clone.play();
+  fadeAudio(clone, clickAudio.duration);
 });
 button1.addEventListener("mouseup", (e) => {
   button1.classList.remove("clicked");
 });
+
+function fadeAudio(audioNode, audioDuration) {
+  let dur = audioDuration.toFixed(1) * 1000;
+  changeAudio(audioNode, 0.8, Math.floor((dur * 1) / 4));
+  changeAudio(audioNode, 0.6, Math.floor((dur * 2) / 4));
+  changeAudio(audioNode, 0.4, Math.floor((dur * 3) / 4));
+  changeAudio(audioNode, 0.2, Math.floor((dur * 4) / 4));
+}
+
+function changeAudio(node, vol, dur) {
+  setTimeout((node.volumne = vol), dur);
+}
