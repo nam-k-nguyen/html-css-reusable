10e77c73d817cc6dfa110aa63d29c4d9f4229a1d
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-03-31 20:04:43 -0400

    Finish button 1

--- buttons/button.js
+++ buttons/button.js
@@ -1,4 +1,5 @@
 const button1 = document.querySelector("#button-one");
+const button2 = document.querySelector("#button-two");
 
 button1.addEventListener("mousedown", (e) => {
   button1.classList.add("clicked");
